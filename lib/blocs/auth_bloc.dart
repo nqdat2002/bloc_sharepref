2e506ffacb14d2f84f02178810984fc1ce01e0f5
Nguyễn Quốc Đạt <datfeed.2706@gmail.com>
2025-10-03 18:54:07 +0700

    add method login & signup for authBlock

--- lib/blocs/auth_bloc.dart
+++ lib/blocs/auth_bloc.dart
@@ -2,18 +2,42 @@
 import 'package:bloc/bloc.dart';
 import 'package:bloc_sharepref/repositories/auth_repository.dart';
 
-abstract class AuthEvent {
-}
-class AppStarted extends AuthEvent{}
-class LoggedIn extends AuthEvent{}
-class LoggedOut extends AuthEvent{}
+abstract class AuthEvent {}
 
-abstract class AuthState {
+class AppStarted extends AuthEvent {}
+
+class LoggedIn extends AuthEvent {}
+
+class LoggedOut extends AuthEvent {}
+
+class LogInRequested extends AuthEvent {
+  final String username;
+  final String password;
+
+  LogInRequested({required this.username, required this.password});
 }
+
+class SignUpRequested extends AuthEvent {
+  final String username;
+  final String password;
+
+  SignUpRequested({required this.username, required this.password});
+}
+
+abstract class AuthState {}
 
 class AuthInitial extends AuthState {}
+
 class Authenticated extends AuthState {}
+
 class Unauthenticated extends AuthState {}
+
+class AuthLoading extends AuthState {}
+
+class AuthFailure extends AuthState {
+  final String message;
+  AuthFailure(this.message);
+}
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final AuthRepository authRepository;
@@ -31,6 +55,37 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     on<LoggedOut>((event, emit) async {
       await authRepository.logOut();
       emit(Unauthenticated());
+    });
+
+    on<LogInRequested>((event, emit) async {
+      emit(AuthLoading());
+      try {
+        final success = await authRepository.login(event.username, event.password);
+        if (success) {
+          emit(Authenticated());
+        } else {
+          emit(AuthFailure("Login failed"));
+        }
+      } catch (e) {
+        emit(AuthFailure(e.toString()));
+      }
+    });
+
+    on<SignUpRequested>((event, emit) async {
+      emit(AuthLoading());
+      try {
+        final success = await authRepository.signUp(
+          event.username,
+          event.password,
+        );
+        if (success) {
+          emit(Authenticated());
+        } else {
+          emit(AuthFailure("Sign up failed"));
+        }
+      } catch (e) {
+        emit(AuthFailure(e.toString()));
+      }
     });
   }
 }
