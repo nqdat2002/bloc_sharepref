eda329791fd6bd14e913b62cee72e8e8629f1cc9
Nguyễn Quốc Đạt <datfeed.2706@gmail.com>
2025-10-04 17:03:26 +0700

    page switcher

---
+++ lib/screens/auth_page_switcher.dart
@@ -0,0 +1,27 @@
+import 'package:bloc_sharepref/screens/login_screen.dart';
+import 'package:bloc_sharepref/screens/signup_screen.dart';
+import 'package:flutter/material.dart';
+
+class AuthPageSwitcher extends StatefulWidget {
+  const AuthPageSwitcher({super.key});
+
+  @override
+  State<AuthPageSwitcher> createState() => _AuthPageSwitcherState();
+}
+
+class _AuthPageSwitcherState extends State<AuthPageSwitcher> {
+  bool showLogin = true;
+
+  void toggle() {
+    setState(() {
+      showLogin = !showLogin;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return showLogin
+        ? LoginScreen(onSwitchToSignUp: toggle)
+        : SignUpScreen(onSwitchToLogin: toggle);
+  }
+}
