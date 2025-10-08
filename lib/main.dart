4acfeab62df5d62256aa0c892434079211b09980
Nguyễn Quốc Đạt <datfeed.2706@gmail.com>
2025-10-08 21:06:32 +0700

    add empty screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,50 @@
+import 'dart:convert';
+
 import 'package:bloc_sharepref/blocs/authentication_bloc.dart';
 import 'package:bloc_sharepref/screens/auth_page_switcher.dart';
+import 'package:bloc_sharepref/screens/empty_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'blocs/auth_bloc.dart' hide AuthInitial;
 import 'repositories/auth_repository.dart';
 import 'screens/home_screen.dart';
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  FirebaseMessaging.instance.getToken().then((value) {
+    print("Firebase Messaging Token: $value");
+  });
+
+  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) async {
+    print("On Message: $message");
+    Navigator.pushNamed(navigatorKey.currentContext!, '/push-page', arguments: {
+      'message': json.encode(message.data),
+    });
+  });
+
+
+  FirebaseMessaging.instance.getInitialMessage().then((message) {
+    if (message != null) {
+      print("Initial Message: $message");
+      Navigator.pushNamed(navigatorKey.currentContext!, '/push-page', arguments: {
+        'message': json.encode(message.data),
+      });
+    }
+  });
+
+  FirebaseMessaging.onBackgroundMessage((_firebaseHandler));
+
   runApp(const MyApp());
+}
+
+Future<void> _firebaseHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("Handling a background message: ${message.messageId}");
 }
 
 class MyApp extends StatefulWidget {
@@ -46,6 +83,11 @@ class _MyAppState extends State<MyApp> {
         // Các bloc khác nếu có
       ],
       child: MaterialApp(
+        navigatorKey: navigatorKey,
+        routes: {
+          '/': (context) => const EmptyScreen(),
+          '/push-page': (context) => const HomeScreen(),
+        },
         home: BlocBuilder<AuthBloc, AuthState>(
           builder: (context, state) {
             if (state is AuthInitial) {
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,8 +2,29 @@ import 'package:bloc_sharepref/blocs/auth_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String message = "";
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    final arguments = ModalRoute.of(context)?.settings.arguments;
+    if (arguments != null && arguments is String) {
+      Map? pushArguments = arguments as Map?;
+
+      setState(() {
+        message = pushArguments?['message'] ?? "";
+      });
+    }
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +37,13 @@ class HomeScreen extends StatelessWidget {
             onPressed: () {
               context.read<AuthBloc>().add(LoggedOut());
             },
+          ),
+
+          IconButton(
+            onPressed: (){
+
+            }, 
+            icon: const Icon(Icons.notifications),
           )
         ],
       ),
